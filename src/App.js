b0137770f884d0e47bdbf52296d862bc42a74e12
Marcelo <marcelo_4940@hotmail.com>
2020-11-05 10:30:44 -0300

    Estrutura página de consulta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, makeStyles, TextField, Typography } from '@material-ui/core';
 import styled from 'styled-components';
 import './App.css';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
-const Button = styled.button`
+const SearchButton = styled.button`
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
@@ -10,7 +13,7 @@ const Button = styled.button`
   background: #004d7a;
   border: 1px solid ${props => props.theme.main};
 `;
-Button.defaultProps = {
+SearchButton.defaultProps = {
   theme: {
     main: "transparent"
   }
@@ -32,20 +35,110 @@ Input.defaultProps = {
 
 const Container = styled.input``;
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    margin: 15
+  },
+  media: {
+    height: 140,
+  },
+});
+
 function App() {
+  const classes = useStyles();
   return (
     <div className="Container">
       <div className="Header">
-        <Input></Input>
-        <Button>Buscar</Button>
+        <TextField id="standard-basic" label="Standard" />
+        <Button variant="contained" color="primary">Buscar</Button>
       </div>
       <div className="Body">
-        <div>Card 1</div>
-        <div>Card 2</div>
-        <div>Card 3</div>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+      </Card>
+
+      <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+      </Card>
+
+      <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+      </Card>
       </div>
       <div className="Footer">
-        Footer
+        <span style={{padding: "5px"}}>Created by Marcelo Luiz Jung</span>
+        <GitHubIcon style={{padding: "5px"}}></GitHubIcon>
+        <LinkedInIcon style={{padding: "5px"}}></LinkedInIcon>
       </div>
     </div>
   );
