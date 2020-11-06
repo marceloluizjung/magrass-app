6b3b990175860911e56e47875414bda3773dd2e3
Marcelo <marcelo_4940@hotmail.com>
2020-11-06 10:06:19 -0300

    Implementação loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, makeStyles, TextField, Typography } from '@material-ui/core';
-import styled from 'styled-components';
-import './App.css';
+import { Button, Card, CardActionArea, CardContent, CardMedia, CircularProgress, makeStyles, TextField, Typography } from '@material-ui/core';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import Axios from 'axios';
 import React, { useEffect, useState } from "react";
+import styled from 'styled-components';
+import './App.css';
 
 const SearchButton = styled.button`
   font-size: 1em;
@@ -54,80 +54,132 @@ function App() {
   let [valueInput, setValueInput] = useState('');
   let [filter, setFilter] = useState('');
   let [city, setCity] = useState('');
+  let [loader, setLoader] = useState('');
   const classes = useStyles();
    
   useEffect(() => {
     if(controll || filter == 'buscar') {
-    getForecast(valueInput).then(({data}) => {
-      setToday(data.forecast.forecastday[0]);
-      setTomorrow(data.forecast.forecastday[1]);
-      setAfterTomorrow(data.forecast.forecastday[2]);
-      setCity(data.location.name);
-      controll = false;
-      setFilter('');
-    },[valueInput]);
+      setLoader(true);
+      getForecast(valueInput).then(({data}) => {
+        setToday(data.forecast.forecastday[0]);
+        setTomorrow(data.forecast.forecastday[1]);
+        setAfterTomorrow(data.forecast.forecastday[2]);
+        setCity(data.location.name);
+        controll = false;
+        setFilter('');
+        setLoader(false);
+      },[valueInput]);
   }
   });
 
   return (
     <div className="Container">
       <div className="Header">
-        <TextField id="standard-basic" label="Standard" onChange={valueInputChange}/>
+        <TextField id="standard-basic" label="Cidade" onChange={valueInputChange}/>
         <Button variant="contained" color="primary" onClick={search}>Buscar</Button>
       </div>
-      <div className="Body">
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={today ? today.day.condition.icon : ''}
-              title="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-              {today ? today.date : ''}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-              <p>{today ? today.day.condition.text : ''}</p>
-              <p>{city}</p>
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-      </Card>
-      <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={tomorrow ? tomorrow.day.condition.icon : ''}
-              title="Contemplative Reptile"/>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-              {tomorrow ? tomorrow.date : ''}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-              <p>{tomorrow ? tomorrow.day.condition.text : ''}</p>
-              <p>{city}</p>
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-      </Card>
-      <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={afterTomorrow ? afterTomorrow.day.condition.icon : ''} title="Contemplative Reptile"/>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-              {afterTomorrow ? afterTomorrow.date : ''}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-              <p>{afterTomorrow ? afterTomorrow.day.condition.text : ''}</p>
-              <p>{city}</p>
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-      </Card>
-      </div>
+      <Rtif boolean={loader}>
+        <div className="Body">
+          <CircularProgress color="secondary"/>
+        </div>
+      </Rtif>
+      <Rtif boolean={!loader}>
+        <div className="Body">
+            <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={today ? today.day.condition.icon : ''}
+                  title="Contemplative Reptile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                  {today ? today.date : ''}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    <div className="Container-data">
+                        <p>{today ? today.day.condition.text : ''}</p>
+                      <div className="Data">
+                        <img width="20" height="20" src="https://www.flaticon.com/svg/static/icons/svg/2041/2041670.svg" alt="High temperature free icon" title="High temperature free icon" class="loaded"></img>
+                        <p>{today ? today.day.maxtemp_c : ''} Cº</p>
+                      </div>
+                      <div className="Data">
+                        <img width="20" height="20" src="https://www.flaticon.com/svg/static/icons/svg/661/661346.svg" alt="Low temperature free icon" title="Low temperature free icon" class="loaded"></img>
+                        <p>{today ? today.day.mintemp_c : ''} Cº</p>
+                      </div>
+                      <div className="Data">
+                        <img width="20" height="20" src="https://www.flaticon.com/svg/static/icons/svg/728/728093.svg" alt="Humidity free icon" title="Humidity free icon" class="loaded"></img>
+                        <p>{today ? today.day.avghumidity : ''} %</p>
+                      </div>
+                      <p>{city}</p>
+                    </div>
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+          </Card>
+          <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={tomorrow ? tomorrow.day.condition.icon : ''}
+                  title="Contemplative Reptile"/>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                  {tomorrow ? tomorrow.date : ''}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                  <div className="Container-data">
+                      <p>{tomorrow ? tomorrow.day.condition.text : ''}</p>
+                    <div className="Data">
+                      <img width="20" height="20" src="https://www.flaticon.com/svg/static/icons/svg/2041/2041670.svg" alt="High temperature free icon" title="High temperature free icon" class="loaded"></img>
+                      <p>{tomorrow ? tomorrow.day.maxtemp_c : ''} Cº</p>
+                    </div>
+                    <div className="Data">
+                      <img width="20" height="20" src="https://www.flaticon.com/svg/static/icons/svg/661/661346.svg" alt="Low temperature free icon" title="Low temperature free icon" class="loaded"></img>
+                      <p>{tomorrow ? tomorrow.day.mintemp_c : ''} Cº</p>
+                    </div>
+                    <div className="Data">
+                      <img width="20" height="20" src="https://www.flaticon.com/svg/static/icons/svg/728/728093.svg" alt="Humidity free icon" title="Humidity free icon" class="loaded"></img>
+                      <p>{tomorrow ? tomorrow.day.avghumidity : ''} %</p>
+                    </div>
+                    <p>{city}</p>
+                  </div>
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+          </Card>
+          <Card className={classes.root}>
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image={afterTomorrow ? afterTomorrow.day.condition.icon : ''} title="Contemplative Reptile"/>
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                  {afterTomorrow ? afterTomorrow.date : ''}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                  <div className="Container-data">
+                      <p>{afterTomorrow ? afterTomorrow.day.condition.text : ''}</p>
+                    <div className="Data">
+                      <img width="20" height="20" src="https://www.flaticon.com/svg/static/icons/svg/2041/2041670.svg" alt="High temperature free icon" title="High temperature free icon" class="loaded"></img>
+                      <p>{afterTomorrow ? afterTomorrow.day.maxtemp_c : ''} Cº</p>
+                    </div>
+                    <div className="Data">
+                      <img width="20" height="20" src="https://www.flaticon.com/svg/static/icons/svg/661/661346.svg" alt="Low temperature free icon" title="Low temperature free icon" class="loaded"></img>
+                      <p>{afterTomorrow ? afterTomorrow.day.mintemp_c : ''} Cº</p>
+                    </div>
+                    <div className="Data">
+                      <img width="20" height="20" src="https://www.flaticon.com/svg/static/icons/svg/728/728093.svg" alt="Humidity free icon" title="Humidity free icon" class="loaded"></img>
+                      <p>{afterTomorrow ? afterTomorrow.day.avghumidity : ''} %</p>
+                    </div>
+                    <p>{city}</p>
+                  </div>
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+          </Card>
+        </div>
+      </Rtif>
       <div className="Footer">
         <span style={{padding: "5px"}}>Created by Marcelo Luiz Jung</span>
         <GitHubIcon style={{padding: "5px"}}></GitHubIcon>
@@ -140,7 +192,8 @@ function App() {
     setValueInput(event.target.value);
   }
 
-  function search(event) {
+  function search() {
+    setLoader(true);
     setFilter('buscar');
   }
 
@@ -154,3 +207,12 @@ function App() {
 }
 
 export default App;
+
+export function Rtif({boolean, ...props}) {
+  const { children } = props;
+  if (boolean)
+      return (
+              {...children}
+      );
+  return null;
+}
